b4e3d8e65c8f69e0f8e8166a3076ccdadff96189
dev-xero <ssh.xero@gmail.com>
2025-12-13 00:38:41 +0100

    day 7: solve part b
    Signed-off-by: dev-xero <ssh.xero@gmail.com>

--- day-07/b.cpp
+++ day-07/b.cpp
@@ -3,15 +3,35 @@
 #include <string>
 #include <vector>
 
-auto read_input(std::ifstream& in) {
-    return "";
+std::vector<std::string> read_input(std::ifstream& in) {
+    std::vector<std::string> manifold;
+    std::string line;
+    while (std::getline(in, line)) {
+        manifold.push_back(line);
+    }
+    return manifold;
 }
 
-long long solve(auto input) {
-    return 0ll; 
+long long solve(std::vector<std::string> input) {
+    int m = input.size();
+    int n = input[0].length();
+    std::vector<long long> columns(n, 1ll);
+    for (int row = m - 2; row > 1; row -= 2) {
+        for (int col = 0; col < n; col++) {
+            if (input[row][col] == '^') {
+                columns[col] = columns[col - 1] + columns[col + 1];
+            }
+        }
+    }
+    return columns[n / 2];
 }
 
 int main() {
     std::ios::sync_with_stdio(0);
     std::cin.tie(0);
+    
+    std::ifstream in("day-07/input.txt");
+    std::vector<std::string> input = read_input(in);
+    
+    std::cout << "timelines: " << solve(input) << std::endl;
 }
